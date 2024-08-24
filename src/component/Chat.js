929f8a04438fb4ca0b829c55aa5ecc8a98019d67
Rlack97 <amalilith97@gmail.com>
2024-08-24 21:22:18 +0900

    chatting page

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,8 +1,68 @@
+import styles from "./Chat.module.css"; // CSS 모듈을 가져옵니다.
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 export default function Chat() {
-    return (
-      <div>
-        <div>채팅페이지</div>
+  const [searchResult, setSearchResult] = useState([]);
+  const [selectFinished, setSelectFinished] = useState(false);
+  const [selectItem, setSelectItem] = useState({});
+  const location = useLocation();
+
+  useEffect(() => {
+    // 가져온 검색 결과 설정
+    setSearchResult(location.state.result || []);
+  }, [location.state.result]);
+
+  const selectBus = (index, item) => {
+    const sumItem = { ...item, index: index };
+    // 서버로 index 날림.
+    setSelectItem(sumItem);
+    setSelectFinished(true);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.firstBalloon}>
+        <div>{searchResult.length}개의 버스가 있습니다.</div>
       </div>
-    );
-  }
-  
+      {searchResult.map((item, index) => (
+        <div
+          key={index}
+          className={styles.firstBalloon}
+          style={{ animationDelay: `${(index + 1) * 0.2}s` }}
+        >
+          <div>
+            출발 : {item.departTime} ~ 도착 : {item.arrivalTime}
+          </div>
+          <div>{item.price}원</div>
+
+          <button
+            className={`${styles.btn} ${
+              selectFinished ? styles.disappear : ""
+            }`}
+            onClick={() => {
+              selectBus(index, item);
+            }}
+          >
+            선택
+          </button>
+        </div>
+      ))}
+      {selectFinished ? (
+        <div className={styles.secondBalloon}>
+          <div>{selectItem.index + 1}번째 버스 선택</div>
+        </div>
+      ) : (
+        <span></span>
+      )}
+
+      {selectFinished ? (
+        <div className={styles.firstBalloon} style={{ animationDelay: "0.2s" }}>
+          <div>결제정보를 요청합니다.</div>
+        </div>
+      ) : (
+        <span></span>
+      )}
+    </div>
+  );
+}
--- src/component/Landing.js
+++ src/component/Landing.js
@@ -1,9 +1,10 @@
-import "./Landing.css";
+import styles from "./Landing.module.css";
 import { useNavigate } from "react-router-dom";
 import { useState, useRef } from "react";
 
 export default function Landing() {
   const navigate = useNavigate();
+  const [isCenter, setIsCenter] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
   const mediaRecorderRef = useRef(null);
   const chunks = useRef([]);
@@ -13,10 +14,10 @@ export default function Landing() {
       // 녹음 중이면 녹음 중지
       mediaRecorderRef.current.stop();
       setIsRecording(false);
-      navigate("/chat");
     } else {
       // 녹음 중이 아니면 녹음 시작
       setIsRecording(true);
+      setIsCenter(true);
 
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       const mediaRecorder = new MediaRecorder(stream);
@@ -29,26 +30,21 @@ export default function Landing() {
       mediaRecorder.onstop = async () => {
         const blob = new Blob(chunks.current, { type: "audio/wav" });
         const formData = new FormData();
-        formData.append("audio", blob, "recording.wav");
+        formData.append("voice", blob, "recording.wav");
 
         try {
-          const response = await fetch(
-            "https://your-server-endpoint.com/upload",
-            {
-              method: "POST",
-              body: formData,
-            }
-          );
+          const response = await fetch("/api/bus/available", {
+            method: "POST",
+            body: formData,
+          });
 
           if (!response.ok) {
             throw new Error(`Server responded with ${response.status}`);
           }
 
           const result = await response.json();
-          console.log("Server response:", result);
-          // 서버 반응 로그
-
           chunks.current = []; // 다음 기록을 위한 청크 리셋
+          navigate("/chat", { state: { result: result } });
         } catch (error) {
           console.error("Error uploading audio:", error);
         }
@@ -59,24 +55,24 @@ export default function Landing() {
   };
 
   return (
-    <div className="baseContainer">
-      <div className="container">
-        <div className="title">버스 매표소</div>
+    <div className={styles.baseContainer}>
+      <div className={styles.container}>
+        <div className={styles.title}>버스 매표소</div>
       </div>
       <div
-        className={`enter ${isRecording ? "centered" : ""}`}
+        className={`${styles.enter} ${isCenter ? styles.centered : ""}`}
         onClick={handleClick}
       >
-        <div className="title">
-          {isRecording ? "녹음 중입니다" : "버스 찾기"}
+        <div className={styles.title}>
+          {isCenter ? "녹음 중입니다" : "버스 찾기"}
         </div>
       </div>
       {isRecording ? (
-        <div className="notification show">
+        <div className={`${styles.notification} ${styles.show}`}>
           출발지, 목적지, 날짜, 좌석을 말씀해주세요
         </div>
       ) : (
-        <div className="notification"></div>
+        <div className={styles.notification}></div>
       )}
     </div>
   );
